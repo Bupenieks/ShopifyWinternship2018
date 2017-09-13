4dd34546f5c5acac32db6ffd518d7108edb1813a
Ben Upenieks <ben.upenieks@gmail.com>
2017-09-12 21:13:07 -0400

    Backend solution working as expected. Needs testing

--- BackendProblem/index.js
+++ BackendProblem/index.js
@@ -1,116 +1,133 @@
-'use strict';
+"use strict";
 
-const http = require('http'),
-    express = require('express'),
-    path = require('path'),
-    request = require('request');
+const http = require("http"),
+  express = require("express"),
+  path = require("path"),
+  request = require("request");
 
 const app = express(),
-    server = http.Server(app);
+  server = http.Server(app);
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'html');
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "html");
 
-server.listen(app.get('port'), () => {
-    console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
 });
 
-
-const apiEndpoint = "https://backend-challenge-winter-2017.herokuapp.com/customers.json"
-let invalidCustomers = []
-let customerDictionairy = {}
-
-const updateCustomerDictionairy = customers => {
-    for (let customer in customers) {
-        //
-        customer["isValidated"] = false
-        for (let key in customer) {
-            if (!customerDictionairy[key]) {
-                customerDictionairy[key] = [customer]
-            } else {
-                customerDictionairy[key].push(customer)
-            }
-        }
-    }
-}
+const apiEndpoint =
+  "https://backend-challenge-winter-2017.herokuapp.com/customers.json";
+let invalidCustomers = [];
 
 const retrieveApiData = page => {
-    return new Promise((resolve, reject) => {
-        request(apiEndpoint + "?page=" + page, (err, result, html) => {
-            if (err) {
-                reject(err)
-            } else {
-                let constraints = result["validations"]
-                let customers = result["customers"]
-                updateCustomerDictionairy(customers)
-                resolve(constraints, customers)
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    request(apiEndpoint + "?page=" + page, (err, result, html) => {
+      console.log(apiEndpoint + "?page=" + page);
+      if (err) {
+        reject(err);
+      } else {
+        let body = JSON.parse(html),
+          constraints = body["validations"],
+          customers = body["customers"];
+        resolve({
+          constraints: constraints,
+          customers: customers
+        });
+      }
+    });
+  });
+};
 
-const validate = (constraints, customers) => {
-    // Iterate through each customer and validate each constaint key
-    for (let customer in customers) {
-        for (let contraint in constraints) {
-            for (let key in constraint) {
-                if (customer[key]) {
-                    switch (key) {
-                        case "type":
-                            if (typeof customer[key] != contraint[key]) markAsInvalid(key, customer)
-                            break
-                        case "length":
-                            let keyLength = customer[key].length
-                            if (keyLength > contraint[key]["max"] || keyLength < contraint[key]["min"])
-                                markAsInvalid(key, customer)
-                            break
-                        default:
-                            console.err("Invalid key detected")
-                    }
-                } else if (contraint["required"]) {
-                    markAsInvalid("required", customer)
+const validate = data => {
+  // Iterate through each customer and validate each constraint key
+  let customers = data.customers,
+    constraints = data.constraints;
+
+  console.log(customers);
+
+  for (let customer of customers) {
+    for (let constraint of constraints) {
+      for (let key in constraint) {
+        if (customer[key] != null) {
+          let constraintAttributes = constraint[key];
+          for (let attribute in constraintAttributes) {
+            switch (attribute) {
+              case "type":
+                if (typeof customer[key] != constraintAttributes[attribute]) {
+                  markAsInvalid(key, customer);
                 }
+                break;
+              case "length":
+                let keyLength = customer[key].length,
+                  maxLength = constraintAttributes[attribute]["max"],
+                  minLength = constraintAttributes[attribute]["min"];
+
+                if (
+                  (maxLength && keyLength > maxLength) ||
+                  (minLength && keyLength < minLength)
+                ) {
+                  markAsInvalid(key, customer);
+                }
+                break;
             }
+          }
+        } else if (constraint[key]["required"]) {
+          markAsInvalid(key, customer);
         }
+      }
     }
+  }
+};
 
-}
+const markAsInvalid = (key, customer) => {
+  invalidCustomers.push([key, customer.id]);
+};
 
-app.get('/validate/', (req, res) => {
+app.get("/validate/", (req, res) => {
+  request(apiEndpoint + "?page=1", (err, result, html) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      let body = JSON.parse(html),
+        constraints = body["validations"],
+        customers = body["customers"],
+        pagination = body["pagination"];
 
-    request(apiEndpoint, (err, result, html) => {
-        if (err) {
-            res.status(400).send(err)
-        } else {
-            let body = JSON.parse(html),
-                constraints = body["validations"],
-                customers = body["customers"],
-                pagination = body["pagination"];
+      if (!customers) {
+        console.log("No customers");
+        res.send({});
+        return;
+      }
 
-            if (!customers) {
-                console.log("No customers")
-                res.send({})
-                return
+      validate({
+        constraints: constraints,
+        customers: customers
+      });
+
+      let numPages = Math.ceil(
+          body["pagination"]["total"] / body["pagination"]["per_page"]
+        ),
+        requestCount = 1,
+        caught = false;
+
+      for (let i = 2; i <= numPages; i++) {
+        retrieveApiData(i)
+          .then(validate)
+          .then(() => {
+            if (++requestCount == numPages && !caught) {
+              res.send(invalidCustomers);
             }
-
-            validate(constraints, customers)
-
-            for (let i = 1; i < body["pagination"]["total"]; i++) {
-                retrieveApiData(i)
-                    .then(validate)
-                    .catch(msg => res.status(400).send(msg));
-            }
-            res.send(body)
-        }
-
-
-    })
-})
-
-
-app.use((req, res) => {
-    res.status(404).send({ url: req.url });
+          })
+          .catch(msg => {
+            res.status(400).send(msg);
+            caught = true;
+          });
+      }
+    }
+  });
 });
 
-
+app.use((req, res) => {
+  res.status(404).send({ url: req.url });
+});
